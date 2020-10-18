04c3f1c1d87d516594065c5dac66ba6041b40b92
AlejoWilches0112 <cawilchesfe@poligran.edu.co>
2020-10-18 15:01:25 -0500

    Vista de Imagenes, Boton para abrir una nueva pestaña con el valor de download_url y Paginador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { GaleriaComponent } from './galeria/galeria.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceImagenService } from './service-imagen.service';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import { ServiceImagenService } from './service-imagen.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatPaginatorModule
   ],
   providers: [ServiceImagenService],
   bootstrap: [AppComponent]
---
+++ src/app/galeria/galeria.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ServiceImagenService } from '../service-imagen.service';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css']
+})
+export class GaleriaComponent implements OnInit {
+
+  imageslist = [];
+  imageListView = [];
+  //Variables de Paginación
+  Length = 0;
+  PageSize = 10;
+  constructor(private service:ServiceImagenService ) { }
+
+  ngOnInit(): void {
+    this.listImages();
+  }
+
+  listImages() {
+    this.service.listImages().subscribe((res: any) => {
+      console.log(res);
+      this.imageslist = res;
+      /* Se insertan las primeras imagenes a mostrar*/
+      for (let i = 0; i < this.PageSize; i++){
+        this.imageListView.push(this.imageslist[i]);
+      }
+      this.Length = this.imageslist.length;
+    });
+  }
+
+  openLink(UrlImage) {
+    window.open(UrlImage, '_blank');
+  }
+
+  changePage(e) {
+    this.PageSize = e.pageSize;
+    this.imageListView = [];
+    for (let i = this.PageSize * e.pageIndex; i < this.PageSize * (e.pageIndex + 1); i++){
+      this.imageListView.push(this.imageslist[i]);
+    }
+  }
+}
